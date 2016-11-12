1c2e78a82d854d6bf6f962b4f5c24ea134f3885a
komatsu <a1602015@sendai-nct.jp>
2016-11-13 02:31:11 +0900

    tooleap使用可能

---
+++ app/src/main/java/com/example/komatsu/rememberyourself/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.komatsu.rememberyourself;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.tooleap.sdk.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Tooleap tooleap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tooleap = Tooleap.getInstance(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) return true;
+        return super.onOptionsItemSelected(item);
+    }
+}
